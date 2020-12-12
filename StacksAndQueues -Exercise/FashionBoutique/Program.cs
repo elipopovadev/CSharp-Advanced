c01c89c49e7a6b617d65f1bf31c7a86d4916150e
Elena Popova <elipopovadev@gmail.com>
2020-12-12 19:14:58 +0200

    SomeChanges

---
+++ StacksAndQueues -Exercise/FashionBoutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valueOfTheClothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var stack = new Stack<int>(valueOfTheClothes);
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int countRacks = 1;
+            int sumClothesValue = 0;
+            while (stack.Count > 0)
+            {
+                int currentClothesValue = stack.Peek();
+                sumClothesValue += currentClothesValue;
+                if (sumClothesValue <= rackCapacity)
+                {
+                    stack.Pop();
+                }
+
+                else if (sumClothesValue > rackCapacity)
+                {
+                    sumClothesValue = 0;
+                    countRacks++;
+                }
+            }
+
+            Console.WriteLine(countRacks);
+        }
+    }
+}
