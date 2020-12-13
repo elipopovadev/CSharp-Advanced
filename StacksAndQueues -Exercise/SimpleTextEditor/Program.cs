f3ad648bbe43f52d885741518e782c9827512029
Elena Popova <elipopovadev@gmail.com>
2020-12-13 20:02:14 +0200

    SomeChanges

---
+++ StacksAndQueues -Exercise/SimpleTextEditor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var historyForAllTextConditions = new Stack<string>();
+            var sb = new StringBuilder();
+            historyForAllTextConditions.Push(sb.ToString());
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string command = Console.ReadLine();
+                string[] commandArray = command.Split();
+                if (commandArray[0] == "1")
+                {
+                    string someString = commandArray[1];
+                    sb.Append(someString);
+                    historyForAllTextConditions.Push(sb.ToString());
+                }
+
+                else if (commandArray[0] == "2")
+                {
+                    int count = int.Parse(commandArray[1]);
+                    int lengthToErases = count;
+                    int startIndex = sb.Length - count;
+                    sb.Remove(startIndex, lengthToErases);
+                    historyForAllTextConditions.Push(sb.ToString());
+                }
+
+                else if (commandArray[0] == "3")
+                {
+                    int index = int.Parse(commandArray[1]);
+                    char symbol = sb[index - 1];
+                    Console.WriteLine(symbol);
+                }
+
+                else if (commandArray[0] == "4")
+                {
+                    if (historyForAllTextConditions.TryPop(out string currentCondition))
+                    {
+                        sb = sb.Clear();
+                        if (historyForAllTextConditions.TryPeek(out string previousCondition))
+                        {
+                            sb.Append(previousCondition);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
