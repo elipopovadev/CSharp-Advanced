b61c089481143532717c3d7308b711a9805fcd00
Elena Popova <elipopovadev@gmail.com>
2020-12-13 16:36:46 +0200

    SomeChanges

---
+++ StacksAndQueues -Exercise/BalancedParenthesis/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackOpenParentheses = new Stack<char>();
+            string expression = Console.ReadLine();
+            foreach (char currentSymbol in expression)
+            {
+                if (currentSymbol == '{' || currentSymbol == '(' || currentSymbol == '[')
+                {
+                    stackOpenParentheses.Push(currentSymbol);
+                }
+
+                else if (currentSymbol == '}' || currentSymbol == ')' || currentSymbol == ']')
+                {
+                    if(stackOpenParentheses.TryPeek(out char lastSymbolInTheStack))
+                    {
+                        lastSymbolInTheStack = stackOpenParentheses.Peek();
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                    if (lastSymbolInTheStack == '{' && currentSymbol =='}')
+                    {
+                        stackOpenParentheses.Pop();
+                                             
+                    }
+
+                    else if (lastSymbolInTheStack == '(' && currentSymbol == ')')
+                    {
+                        stackOpenParentheses.Pop();
+                       
+                    }
+
+                    else if (lastSymbolInTheStack == '[' && currentSymbol == ']')
+                    {
+                        stackOpenParentheses.Pop();
+                       
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");            
+        }
+    }
+}
