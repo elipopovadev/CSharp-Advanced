46b513646504cf4c2b009a161a8b702d7bc46a17
Elena Popova <elipopovadev@gmail.com>
2020-12-18 10:59:00 +0200

    SomeChanges

---
+++ MultidimensionalArrays-Lab/SumMatrixElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(matrix.Cast<int>().Sum());
+        }
+    }
+}
