30a6593e31ffa0efcfa53031231ff9387a05c7e8
Elena Popova <elipopovadev@gmail.com>
2020-12-18 11:53:44 +0200

    SomeChages

---
+++ MultidimensionalArrays-Lab/PrimaryDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[sizeMatrix, sizeMatrix];
+            int sumDiagonal = 0;
+
+            for (int row = 0; row < sizeMatrix ; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < sizeMatrix; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if (row == col)
+                    {
+                        sumDiagonal += matrix[row, col];
+                    }                
+                }
+            }
+
+            Console.WriteLine(sumDiagonal);
+        }
+    }
+}
