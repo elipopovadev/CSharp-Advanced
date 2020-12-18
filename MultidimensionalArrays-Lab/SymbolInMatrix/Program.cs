abdf0e38ac3aa70b8dbe797aa3c72b3f3fb28aa7
Elena Popova <elipopovadev@gmail.com>
2020-12-18 12:33:14 +0200

    SomeChanges

--- MultidimensionalArrays-Lab/SymbolInMatrix/Program.cs
+++ MultidimensionalArrays-Lab/SymbolInMatrix/Program.cs
@@ -23,11 +23,12 @@ namespace SymbolInMatrix
             {
                 for (int row = 0; row < matrix.GetLength(0); row++)
                 {
-                    for (int col = 0; col < size; col++)
+                    for (int col = 0; col < matrix.GetLength(1); col++)
                     {
                         if (matrix[row, col] == symbol)
                         {
                             Console.WriteLine($"({row}, {col})");
+                            return;
                         }
                     }
                 }
