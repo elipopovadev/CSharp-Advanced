b787c99d58f483e190bcbf8418573a8f3468f7cb
Elena Popova <elipopovadev@gmail.com>
2021-01-02 00:17:05 +0200

    SomeChanges

--- MultidimensionalArrays-Exercise/RadioactiveMutantVampireBunnies/Program.cs
+++ MultidimensionalArrays-Exercise/RadioactiveMutantVampireBunnies/Program.cs
@@ -4,257 +4,153 @@ using System.Collections.Generic;
 
 namespace RadioactiveMutantVampireBunnies
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int rows = input[0];
             int cols = input[1];
-            char[,] matrix = new char[rows, cols];
-            ReadTheMatrix(rows, cols, matrix);
-            int rowPlayerStart = 0;
-            int colPlayerStart = 0;
-            bool haveStart = false;
-            for (int row = 0; row < rows; row++) // validate the matrix
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    if (matrix[row, col] != 'P' && matrix[row, col] != '.' && matrix[row, col] != 'B')
-                    {
-                        return;
-                    }
-
-                    if (matrix[row, col] == 'P')
-                    {
-                        rowPlayerStart = row;
-                        colPlayerStart = col;
-                        haveStart = true;
-                    }
-
-                    else if (haveStart == true && matrix[row, col] == 'P')
-                    {
-                        return;
-                    }
-                }
-            }
-
-            if (haveStart)
-            {
-                char startPlayer = matrix[rowPlayerStart, colPlayerStart];
-            }
-
-            else
-            {
-                return;
-            }
-
-            char[] commandArray = Console.ReadLine().ToArray();
-            int currentRowWithPlayer = 0;
-            int currentColWithPlayer = 0;
-            bool haveBunnyInTheCurrentCell = false;
-
-            for (int i = 0; i < commandArray.Length; i++)
-            {
-                string direction = commandArray[i].ToString();
-                if (direction == 'U'.ToString())
-                {
-                    if (rowPlayerStart - 1 >= 0)
-                    {
-                        currentRowWithPlayer = rowPlayerStart - 1;
-                        currentColWithPlayer = colPlayerStart;
-                        matrix[rowPlayerStart, colPlayerStart] = '.';
-                        if (matrix[currentRowWithPlayer, currentColWithPlayer] == 'B') // the player reached cell with bunny
-                        {
-                            haveBunnyInTheCurrentCell = true;
-                        }
-                    }
-
-                    else if (rowPlayerStart - 1 < 0) matrix[rowPlayerStart, colPlayerStart] = '.';
-                    var stackWithRowsWhereHaveBunnies = new Stack<int>();
-                    var stackWithColsWhereHaveBunnies = new Stack<int>();
-                    SaveWhereAreTheBunnies(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    BunniesMultiply(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    if (rowPlayerStart - 1 < 0) break;
-                    if (rowPlayerStart - 1 >= 0)
-                    {
-                        CheckForBunnyInTheCurrentCell(matrix, ref rowPlayerStart, ref colPlayerStart, currentRowWithPlayer, currentColWithPlayer, ref haveBunnyInTheCurrentCell);
-                    }
-                }
-
-                else if (direction == 'D'.ToString())
-                {
-                    if (rowPlayerStart + 1 < rows)
-                    {
-                        currentRowWithPlayer = rowPlayerStart + 1;
-                        currentColWithPlayer = colPlayerStart;
-                        matrix[rowPlayerStart, colPlayerStart] = '.';
-
-                        if (matrix[currentRowWithPlayer, currentColWithPlayer] == 'B') // the player reached cell with bunny
-                        {
-                            haveBunnyInTheCurrentCell = true;
-                        }
-                    }
-
-                    else if (rowPlayerStart + 1 >= rows) matrix[rowPlayerStart, colPlayerStart] = '.';
-                    var stackWithRowsWhereHaveBunnies = new Stack<int>();
-                    var stackWithColsWhereHaveBunnies = new Stack<int>();
-                    SaveWhereAreTheBunnies(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    BunniesMultiply(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    if (rowPlayerStart + 1 >= rows) break;
-                    if (rowPlayerStart + 1 < rows)
-                    {
-                        CheckForBunnyInTheCurrentCell(matrix, ref rowPlayerStart, ref colPlayerStart, currentRowWithPlayer, currentColWithPlayer, ref haveBunnyInTheCurrentCell);
-                    }
-                }
-
-                else if (direction == 'L'.ToString())
-                {
-                    if (colPlayerStart - 1 >= 0)
-                    {
-                        currentRowWithPlayer = rowPlayerStart;
-                        currentColWithPlayer = colPlayerStart - 1;
-                        matrix[rowPlayerStart, colPlayerStart] = '.';
-                        if (matrix[currentRowWithPlayer, currentColWithPlayer] == 'B') // the player reached cell with bunny
-                        {
-                            haveBunnyInTheCurrentCell = true;
-                        }
-                    }
-
-                    else if (colPlayerStart - 1 < 0) matrix[rowPlayerStart, colPlayerStart] = '.';
-                    var stackWithRowsWhereHaveBunnies = new Stack<int>();
-                    var stackWithColsWhereHaveBunnies = new Stack<int>();
-                    SaveWhereAreTheBunnies(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    BunniesMultiply(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    if (colPlayerStart - 1 < 0) break;
-                    if (colPlayerStart - 1 >= 0)
-                    {
-                        CheckForBunnyInTheCurrentCell(matrix, ref rowPlayerStart, ref colPlayerStart, currentRowWithPlayer, currentColWithPlayer, ref haveBunnyInTheCurrentCell);
-                    }                
-                }
-
-                else if (direction == 'R'.ToString())
-                {
-                    if (colPlayerStart + 1 < cols)
-                    {
-                        currentRowWithPlayer = rowPlayerStart;
-                        currentColWithPlayer = colPlayerStart + 1;
-                        matrix[rowPlayerStart, colPlayerStart] = '.';
-                        if (matrix[currentRowWithPlayer, currentColWithPlayer] == 'B') // the player reached cell with bunny
-                        {
-                            haveBunnyInTheCurrentCell = true;
-                        }
-                    }
-
-                    else if (colPlayerStart + 1 >= cols) matrix[rowPlayerStart, colPlayerStart] = '.';
-                    var stackWithRowsWhereHaveBunnies = new Stack<int>();
-                    var stackWithColsWhereHaveBunnies = new Stack<int>();
-                    SaveWhereAreTheBunnies(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    BunniesMultiply(rows, cols, matrix, stackWithRowsWhereHaveBunnies, stackWithColsWhereHaveBunnies);
-                    if(colPlayerStart + 1 >= cols) break;
-                    if (colPlayerStart + 1 < cols)
-                    {
-                        CheckForBunnyInTheCurrentCell(matrix, ref rowPlayerStart, ref colPlayerStart, currentRowWithPlayer, currentColWithPlayer, ref haveBunnyInTheCurrentCell);
-                    }
-                }
-
-                if (haveBunnyInTheCurrentCell)
-                {
-                    break;
-                }
-            }
-
-            PrintTheResult(rows, cols, matrix, rowPlayerStart, colPlayerStart, currentRowWithPlayer, currentColWithPlayer, haveBunnyInTheCurrentCell);
-        }
-
-
-        private static void CheckForBunnyInTheCurrentCell(char[,] matrix, ref int rowPlayerStart, ref int colPlayerStart, int currentRowWithPlayer, int currentColWithPlayer, ref bool haveBunnyInTheCurrentCell)
-        {
-            if (matrix[currentRowWithPlayer, currentColWithPlayer] == 'B') // bunny reached the player
-            {
-                haveBunnyInTheCurrentCell = true;
-            }
-
-            else if (matrix[currentRowWithPlayer, currentColWithPlayer] != 'B')
-            {
-                matrix[currentRowWithPlayer, currentColWithPlayer] = 'P';
-                rowPlayerStart = currentRowWithPlayer;
-                colPlayerStart = currentColWithPlayer;
-            }
-        }
-
-        private static void SaveWhereAreTheBunnies(int rows, int cols, char[,] matrix, Stack<int> stackWithRowsWhereHaveBunnies, Stack<int> stackWithColsWhereHaveBunnies)
-        {
-            for (int row = 0; row < rows; row++)  // find cells where are the bunnies
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    if (matrix[row, col] == 'B')
-                    {
-                        stackWithRowsWhereHaveBunnies.Push(row);
-                        stackWithColsWhereHaveBunnies.Push(col);
-                    }
-                }
-            }
-        }
-
-        private static void BunniesMultiply(int rows, int cols, char[,] matrix, Stack<int> stackWithRowsWhereHaveBunnies, Stack<int> stackWithColsWhereHaveBunnies)
-        {
-            while (stackWithRowsWhereHaveBunnies.Count > 0 && stackWithColsWhereHaveBunnies.Count > 0) // bunnies multiply very fast
-            {
-                int currentRowWithBunny = stackWithRowsWhereHaveBunnies.Pop();
-                int currentColWithBunny = stackWithColsWhereHaveBunnies.Pop();
-                if (currentRowWithBunny - 1 >= 0) matrix[currentRowWithBunny - 1, currentColWithBunny] = 'B'; // up
-                if (currentRowWithBunny + 1 < rows) matrix[currentRowWithBunny + 1, currentColWithBunny] = 'B'; // down
-                if (currentColWithBunny - 1 >= 0) matrix[currentRowWithBunny, currentColWithBunny - 1] = 'B'; // left
-                if (currentColWithBunny + 1 < cols) matrix[currentRowWithBunny, currentColWithBunny + 1] = 'B'; // right
-            }
-        }
-
-        private static void PrintTheResult(int rows, int cols, char[,] matrix, int rowPlayerStart, int colPlayerStart, int currentRowWithPlayer, int currentColWithPlayer, bool haveBunnyInTheCurrentCell)
-        {
-            if (haveBunnyInTheCurrentCell)
-            {
-                for (int row = 0; row < rows; row++)
-                {
-                    for (int col = 0; col < cols; col++)
-                    {
-                        Console.Write(matrix[row, col]);
-                    }
-
-                    Console.WriteLine();
-
-                }
-
-                Console.WriteLine($"dead: { currentRowWithPlayer} { currentColWithPlayer}");
-            }
-
-            else
-            {
-                for (int row = 0; row < rows; row++)
-                {
-                    for (int col = 0; col < cols; col++)
-                    {
-                        Console.Write(matrix[row, col]);
-                    }
-
-                    Console.WriteLine();
-
-                }
-
-                Console.WriteLine($"won: {rowPlayerStart} {colPlayerStart}");
-            }
-        }
-
-        private static void ReadTheMatrix(int rows, int cols, char[,] matrix)
-        {
+            var matrix = new char[rows, cols];
+            int rowStart = 0;
+            int colStart = 0;
             for (int row = 0; row < rows; row++)
             {
                 string currentLine = Console.ReadLine();
                 for (int col = 0; col < cols; col++)
                 {
                     matrix[row, col] = currentLine[col];
+                    if (matrix[row, col] == 'P')
+                    {
+                        rowStart = row;
+                        colStart = col;
+                    }
                 }
+            }
+
+            string commandLine = Console.ReadLine();
+            foreach (var command in commandLine)
+            {
+                int currentRow = rowStart;
+                int currentCol = colStart;
+                switch (command)
+                {
+                    case 'U':
+                        currentRow--;
+                        break;
+                    case 'D':
+                        currentRow++;
+                        break;
+                    case 'L':
+                        currentCol--;
+                        break;
+                    case 'R':
+                        currentCol++;
+                        break;
+                }
+
+                if (currentRow >= 0 && currentRow < rows && currentCol >= 0 && currentCol < cols) // the player is in the lair
+                {
+                    if (matrix[currentRow, currentCol] != 'B') // The player doesn't reach bunny
+                    {
+                        matrix[rowStart, colStart] = '.';
+                        matrix[currentRow, currentCol] = 'P';
+                        var queueWithBunnies = new Queue<List<int>>();
+                        FindWhereAreTheBunnies(rows, cols, matrix, queueWithBunnies);
+                        BunniesMultiply(rows, cols, matrix, queueWithBunnies);
+                        if (matrix[currentRow, currentCol] != 'B') // after the multiply bunny don't reach the player
+                        {
+                            rowStart = currentRow;
+                            colStart = currentCol;
+                            continue;
+                        }
+
+                        else if (matrix[currentRow, currentCol] == 'B') // after the multiply bunny reach the player
+                        {
+                            PrintTheMatrix(rows, cols, matrix);
+                            Console.WriteLine($"dead: {currentRow} {currentCol}");
+                            return;
+                        }
+                    }
+
+                    else if (matrix[currentRow, currentCol] == 'B') // The player reaches bunny
+                    {
+                        var queueWithBunnies = new Queue<List<int>>();
+                        FindWhereAreTheBunnies(rows, cols, matrix, queueWithBunnies);
+                        BunniesMultiply(rows, cols, matrix, queueWithBunnies);
+                        PrintTheMatrix(rows, cols, matrix);
+                        Console.WriteLine($"dead: {currentRow} {currentCol}");
+                        return;
+                    }
+                }
+
+                else // The player escapes from the lair     
+                {
+                    matrix[rowStart, colStart] = '.';
+                    var queueWithBunnies = new Queue<List<int>>();
+                    FindWhereAreTheBunnies(rows, cols, matrix, queueWithBunnies);
+                    BunniesMultiply(rows, cols, matrix, queueWithBunnies);
+                    PrintTheMatrix(rows, cols, matrix);
+                    Console.WriteLine($"won: {rowStart} {colStart}");
+                    return;
+                }
+            }
+        }
+
+
+        private static void BunniesMultiply(int rows, int cols, char[,] matrix, Queue<List<int>> queueWithBunnies)
+        {
+            while (queueWithBunnies.Count > 0)
+            {
+                var bunniesCoordinates = queueWithBunnies.Dequeue();
+                int rowBunnies = bunniesCoordinates[0];
+                int colBunnies = bunniesCoordinates[1];
+                if (rowBunnies - 1 >= 0)
+                {
+                    matrix[rowBunnies - 1, colBunnies] = 'B';
+                }
+
+                if (rowBunnies + 1 < rows)
+                {
+                    matrix[rowBunnies + 1, colBunnies] = 'B';
+                }
+
+                if (colBunnies - 1 >= 0)
+                {
+                    matrix[rowBunnies, colBunnies - 1] = 'B';
+                }
+
+                if (colBunnies + 1 < cols)
+                {
+                    matrix[rowBunnies, colBunnies + 1] = 'B';
+                }
+            }
+        }
+
+        private static void FindWhereAreTheBunnies(int rows, int cols, char[,] matrix, Queue<List<int>> queueWithBunnies)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        queueWithBunnies.Enqueue(new List<int> { row, col });
+                    }
+                }
+            }
+        }
+
+        private static void PrintTheMatrix(int rows, int cols, char[,] matrix)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
             }
         }
     }
---
+++ SetsAndDictionariesAdvanced-Lab/SetsAndDictionariesAdvanced-Lab/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CitiesByContinentAndCountry
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
