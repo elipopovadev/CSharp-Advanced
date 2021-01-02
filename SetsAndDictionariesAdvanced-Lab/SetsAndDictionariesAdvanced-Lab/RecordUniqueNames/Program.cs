6b2313ba187e174c8f1d45c0daf29c27032ef73e
Elena Popova <elipopovadev@gmail.com>
2021-01-02 18:06:59 +0200

    SomeChanges

---
+++ SetsAndDictionariesAdvanced-Lab/SetsAndDictionariesAdvanced-Lab/RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNum = int.Parse(Console.ReadLine());
+            var hashSetUniqueNames = new HashSet<string>();
+            for (int i = 0; i < totalNum; i++)
+            {
+                string name = Console.ReadLine();
+                hashSetUniqueNames.Add(name);
+            }
+
+            foreach (string name in hashSetUniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
