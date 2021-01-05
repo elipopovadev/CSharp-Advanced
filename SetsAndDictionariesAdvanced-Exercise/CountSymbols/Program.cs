584d815d277549f3a6a2dae131e9c0be4145d993
Elena Popova <elipopovadev@gmail.com>
2021-01-05 23:57:15 +0200

    SomeChanges

---
+++ SetsAndDictionariesAdvanced-Exercise/CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var dictSymbolTimes = new Dictionary<char, int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+                if (!dictSymbolTimes.ContainsKey(symbol))
+                {
+                    dictSymbolTimes.Add(symbol, 1);
+                }
+
+                else
+                {
+                    dictSymbolTimes[symbol]++;
+                }
+            }
+
+            foreach (var (symbol, times) in dictSymbolTimes.OrderBy(symbol => symbol.Key))
+            {
+                Console.WriteLine($"{symbol}: {times} time/s");
+            }
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvanced-Exercise/Wardrobe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
