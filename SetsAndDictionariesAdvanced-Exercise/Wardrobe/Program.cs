3d3cebfaee310ad529d9bd9820bbe033e7ca2411
Elena Popova <elipopovadev@gmail.com>
2021-01-06 00:00:11 +0200

    SomeChanges

--- SetsAndDictionariesAdvanced-Exercise/Wardrobe/Program.cs
+++ SetsAndDictionariesAdvanced-Exercise/Wardrobe/Program.cs
@@ -7,7 +7,37 @@ namespace Wardrobe
     {
         static void Main(string[] args)
         {
-            
+            int numberOfLines = int.Parse(Console.ReadLine());   
+            var dictColorClothesCount = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string colour = input[0];
+                string[] secondPartWithClothes = input[1].Split(',');
+                foreach (var cloth in secondPartWithClothes)
+                {
+                    if (!dictColorClothesCount.ContainsKey(colour))
+                    {
+                        dictColorClothesCount[colour] = new Dictionary<string, int>();
+                        dictColorClothesCount[colour].Add(cloth, 1);
+                    }
+
+                    else if (dictColorClothesCount.ContainsKey(colour))
+                    {
+                        if (!dictColorClothesCount[colour].ContainsKey(cloth))
+                        {
+                            dictColorClothesCount[colour].Add(cloth, 0);
+                        }
+
+                        dictColorClothesCount[colour][cloth]++;
+                    }
+                }
+            }
+
+            string[] inputLookingFor = Console.ReadLine().Split();
+            string colorLookingFor = inputLookingFor[0];
+            string clothLookingFor = inputLookingFor[1];
+           
         }
     }
 }
