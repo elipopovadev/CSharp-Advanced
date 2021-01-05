1fc30e27e795baa4e431c46cfa0bb49befe57b61
Elena Popova <elipopovadev@gmail.com>
2021-01-05 13:55:47 +0200

    SomeChanges

--- SetsAndDictionariesAdvanced-Exercise/UniqueUsernames/Program.cs
+++ SetsAndDictionariesAdvanced-Exercise/UniqueUsernames/Program.cs
@@ -8,14 +8,14 @@ namespace UniqueUsernames
         static void Main(string[] args)
         {           
             int number = int.Parse(Console.ReadLine());
-            var hashSetWithUserNames = new HashSet<string>();
+            var setWithUserNames = new HashSet<string>();
             for (int i = 0; i < number; i++)
             {
                 string userName = Console.ReadLine();
-                hashSetWithUserNames.Add(userName);
+                setWithUserNames.Add(userName);
             }
 
-            Console.WriteLine(string.Join(Environment.NewLine, hashSetWithUserNames));
+            Console.WriteLine(string.Join(Environment.NewLine, setWithUserNames));
         }
     }
 }
