a7dd72c30bbd921864f006c5d37443080bbf82af
Elena Popova <elipopovadev@gmail.com>
2021-01-06 00:04:18 +0200

    SomeChanges

---
+++ SetsAndDictionariesAdvanced-Exercise/TheV-Logger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheV_Logger
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
