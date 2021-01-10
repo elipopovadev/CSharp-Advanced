084aca6a4d6b2651cb4536e5e499d46cf50b40dc
Elena Popova <elipopovadev@gmail.com>
2021-01-10 02:57:47 +0200

    SomeChanges

--- SetsAndDictionariesAdvanced-Exercise/MOBAChallenger-withClass/Program.cs
+++ SetsAndDictionariesAdvanced-Exercise/MOBAChallenger-withClass/Program.cs
@@ -12,9 +12,9 @@ namespace MOBAChallenger_withClass
             string command;
             while ((command = Console.ReadLine()) != "Season end")
             {
-                string[] commandArray = command.Split(" -> ",' ');
-                if (!commandArray.Contains("vs"))
+                if (!command.Contains("vs"))
                 {
+                    string[] commandArray = command.Split(" -> ");
                     string playerName = commandArray[0];
                     string position = commandArray[1];
                     int skill = int.Parse(commandArray[2]);
@@ -41,86 +41,93 @@ namespace MOBAChallenger_withClass
                     }
                 }
 
-                else if (commandArray.Contains("vs"))
+                else if (command.Contains("vs"))
                 {
+                    string[] commandArray = command.Split(' ');
                     string firstPlayer = commandArray[0];
                     string secondPlayer = commandArray[2];
-                    if(!dictPlayerPositionSkill.ContainsKey(firstPlayer) || !dictPlayerPositionSkill.ContainsKey(secondPlayer))
+                    if (!dictPlayerPositionSkill.ContainsKey(firstPlayer) || !dictPlayerPositionSkill.ContainsKey(secondPlayer))
                     {
                         continue;
                     }
 
                     bool firstPlayerIsWinner = false;
                     bool secondPlayerIsWinner = false;
-                    string commanPosition = string.Empty;
-                    int currentSkill = 0;
                     foreach (var (position1, skill1) in dictPlayerPositionSkill[firstPlayer].DictPositionSkill)
                     {
                         foreach (var (position2, skill2) in dictPlayerPositionSkill[secondPlayer].DictPositionSkill)
                         {
-                            if (position1 == position2 && skill1>skill2)
+                            if (position1 == position2 && skill1 > skill2)
                             {
-                                commanPosition = position1;
-                                currentSkill = skill2;
                                 firstPlayerIsWinner = true;
                                 break;
                             }
 
-                            else if(position1==position2 && skill2 > skill1)
+                            else if (position1 == position2 && skill2 > skill1)
                             {
-                                commanPosition = position1;
-                                currentSkill = skill1;
                                 secondPlayerIsWinner = true;
                                 break;
                             }
                         }
 
-                        if(firstPlayerIsWinner=true || secondPlayerIsWinner == true)
+                        if (firstPlayerIsWinner == true || secondPlayerIsWinner == true)
                         {
                             break;
                         }
                     }
 
-                    if (firstPlayerIsWinner)
+                    if (firstPlayerIsWinner == false && secondPlayerIsWinner == false)
                     {
-                        dictPlayerPositionSkill.Remove(secondPlayer);
-                        dictPlayerPositionSkill[firstPlayer].DictPositionSkill[commanPosition] -= currentSkill;
+                        continue;
                     }
 
-                    if (secondPlayerIsWinner)
-                    {
-                        dictPlayerPositionSkill.Remove(firstPlayer);
-                        dictPlayerPositionSkill[secondPlayer].DictPositionSkill[commanPosition] -= currentSkill;
-                    }
-
-
-
-
-
-
+                    CheckWhoIsTheWinnerAndRemoveLoser(dictPlayerPositionSkill, firstPlayer, secondPlayer, firstPlayerIsWinner, secondPlayerIsWinner);
                 }
-
-
-
-
-
             }
 
-
-
+            PrintResult(dictPlayerPositionSkill);
         }
-    }
 
-    class Player
-    {
-        public string Name { get; set; }
-        public new Dictionary<string, int> DictPositionSkill { get; set; }
 
-        public Player(string name)
+        private static void CheckWhoIsTheWinnerAndRemoveLoser(Dictionary<string, Player> dictPlayerPositionSkill, string firstPlayer, string secondPlayer, bool firstPlayerIsWinner, bool secondPlayerIsWinner)
         {
-            this.Name = name;
-            this.DictPositionSkill = new Dictionary<string, int>();
+            if (firstPlayerIsWinner)
+            {
+                dictPlayerPositionSkill.Remove(secondPlayer);
+            }
+
+            else if (secondPlayerIsWinner)
+            {
+                dictPlayerPositionSkill.Remove(firstPlayer);
+            }
         }
 
+        private static void PrintResult(Dictionary<string, Player> dictPlayerPositionSkill)
+        {
+            foreach (var (currentPlayer, positionSkill) in dictPlayerPositionSkill
+                .OrderByDescending(x => x.Value.DictPositionSkill.Values.Sum()).ThenBy(x => x.Key))
+            {
+
+                Console.WriteLine($"{currentPlayer}: {dictPlayerPositionSkill[currentPlayer].DictPositionSkill.Values.Sum()} skill");
+                foreach (var (position, skill) in dictPlayerPositionSkill[currentPlayer].DictPositionSkill
+                    .OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"- {position} <::> {skill}");
+                }
+            }
+        }
     }
 }
+
+class Player
+{
+    public string Name { get; set; }
+    public Dictionary<string, int> DictPositionSkill { get; set; }
+
+    public Player(string name)
+    {
+        this.Name = name;
+        this.DictPositionSkill = new Dictionary<string, int>();
+    }
+}
+
