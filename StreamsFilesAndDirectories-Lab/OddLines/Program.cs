4044d142f4d4d303caffda2df259f0515a72f9df
Elena Popova <elipopovadev@gmail.com>
2021-01-13 23:32:29 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Lab/OddLines/Program.cs
+++ StreamsFilesAndDirectories-Lab/OddLines/Program.cs
@@ -7,20 +7,24 @@ namespace OddLines
     {
         static void Main(string[] args)
         {
-            using(var reader= new StreamReader($"OddLines.txt"))
+            using (var reader = new StreamReader($"OddLines.txt"))
             {
-                int counter = 0;
-                using(var writer=new StreamWriter("OddLinesAnother.txt"))
+                using (var writer = new StreamWriter("otput.txt"))
                 {
-                    var line = reader.ReadLine();
-                    if (counter % 2 != 0)
+                    int counter = 0;
+                    while (!reader.EndOfStream)
                     {
-                        writer.WriteLine(line);
-                    }
+                        var line = reader.ReadLine();
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
 
-                    counter++;
+                        counter++;
+                    }
                 }
             }
         }
     }
 }
+
