2bd974752961d53948d1588680778d7c6bef4ee1
Elena Popova <elipopovadev@gmail.com>
2021-01-14 18:28:12 +0200

    SomeChanges

---
+++ StreamsFilesAndDirectories-Lab/WordCount/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
