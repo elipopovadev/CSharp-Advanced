54ff299b961abe2f2683a9f345443eef3a787a86
Elena Popova <elipopovadev@gmail.com>
2021-01-14 17:48:29 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Lab/LineNumbers/Program.cs
+++ StreamsFilesAndDirectories-Lab/LineNumbers/Program.cs
@@ -7,7 +7,19 @@ namespace LineNumbers
     {
         static void Main(string[] args)
         {
-         
+            using (var reader = new StreamReader(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\LineNumbers\input.txt"))
+            {
+                using(var writer = new StreamWriter(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\LineNumbers\output.txt"))
+                {
+                    int lineNumber = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        writer.WriteLine($"{lineNumber} {line}");
+                        lineNumber++;
+                    }
+                }
+            }
         }
     }
 }
