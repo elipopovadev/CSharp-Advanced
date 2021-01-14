3d216b6b69c3c310b458a77966fe6ec2bcab75c9
Elena Popova <elipopovadev@gmail.com>
2021-01-14 21:20:39 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Lab/WordCount/Program.cs
+++ StreamsFilesAndDirectories-Lab/WordCount/Program.cs
@@ -12,28 +12,34 @@ namespace WordCount
             var dictWordCount = new Dictionary<string, int>();
             using (var readerWords = new StreamReader(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\WordCount\words.txt"))
             {
-                using (var readerText = new StreamReader(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\WordCount\text.txt"))
                 using (var writerInOutput = new StreamWriter(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\WordCount\output.txt"))
                 {
                     string[] wordsArray = readerWords.ReadToEnd().ToLower().Split(" ");
-                    string[] textArray = readerText.ReadToEnd().ToLower().Split(new char[]{',',' ','-','.','?','!'});
-                    foreach (string wordInWords in  wordsArray)
-                    {
-                        foreach (string wordInText in textArray)
-                        {
-                            if (string.Equals(wordInWords, wordInText))
-                            {
-                                if (!dictWordCount.ContainsKey(wordInWords))
-                                {
-                                    dictWordCount[wordInWords] = 1;
-                                }
 
-                                else
+                    foreach (string wordInWords in wordsArray)
+                    {
+                        using (var readerText = new StreamReader(@"C:\Users\eli\Desktop\AllMyRepos\CSharp-Advanced\StreamsFilesAndDirectories-Lab\WordCount\text.txt"))
+                        {
+                            while (!readerText.EndOfStream)
+                            {
+                                string[] textLine = readerText.ReadLine().ToLower().Split(new char[] {',',' ', '-', '.', '?', '!'});
+                                foreach (string wordInText in textLine)
                                 {
-                                    dictWordCount[wordInWords]++;
-                                }                             
+                                    if (string.Equals(wordInWords, wordInText))
+                                    {
+                                        if (!dictWordCount.ContainsKey(wordInWords))
+                                        {
+                                            dictWordCount[wordInWords] = 1;
+                                        }
+
+                                        else
+                                        {
+                                            dictWordCount[wordInWords]++;
+                                        }
+                                    }
+                                }
                             }
-                        }
+                        }                         
                     }
 
                     var sortedDict = dictWordCount.OrderByDescending(x => x.Value);
