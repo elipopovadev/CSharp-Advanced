786afcd6b50f5a6604ed30b8decc5ec5fae4d61a
Elena Popova <elipopovadev@gmail.com>
2021-01-15 15:37:49 +0200

    SomeChanges

---
+++ StreamsFilesAndDirectories-Lab/FolderSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"..\..\..\TestFolder");
+            long sum = 0;
+            foreach (var file in files)
+            {
+                var currentFileLength= new FileInfo(file).Length;
+                sum += currentFileLength;
+            }
+
+            double size= (double) sum/ 1024 * 1024;
+            File.WriteAllText(@"..\..\..\Result.txt", size.ToString());
+        }
+    }
+}
