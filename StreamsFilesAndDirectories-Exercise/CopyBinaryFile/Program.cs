64ab0d12ab214172f400ffc18b5ac3be5d5efc09
Elena Popova <elipopovadev@gmail.com>
2021-01-17 17:16:58 +0200

    SomeChanges

---
+++ StreamsFilesAndDirectories-Exercise/CopyBinaryFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream reader = new FileStream(@"..\..\..\university.png", FileMode.Open, FileAccess.Read))
+            using (FileStream writer = new FileStream(@"..\..\..\newFile.png", FileMode.Create, FileAccess.Write))
+            {
+                reader.CopyTo(writer);
+            }
+        }
+    }
+}
