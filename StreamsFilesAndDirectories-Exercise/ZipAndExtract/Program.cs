71f76ca1908f62ba768b8260822304a73db53af7
Elena Popova <elipopovadev@gmail.com>
2021-01-18 00:09:51 +0200

    SomeChanges

---
+++ StreamsFilesAndDirectories-Exercise/ZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"F:\C#\ZipFileDemoC#Advanced";
+            string zipPath = @"F:\C#\ZipFileDemoCreateZip\result.zip"; // result.zip
+            string extractPath = @"F:\C#\ZipFileDemoExtractZip\";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+            ZipFile.ExtractToDirectory(zipPath, extractPath);       
+        }
+    }
+}
---
+++ StreamsFilesAndDirectories-Exercise/ZipAndExtract/StorageFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ZipAndExtract
+{
+    internal class StorageFile
+    {
+        internal Task<Stream> OpenStreamForWriteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
