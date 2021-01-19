7a823886fddd5ceaa3596989fb4114fd203f62d9
Elena Popova <elipopovadev@gmail.com>
2021-01-19 23:29:48 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Exercise/DirectoryTraversal/Program.cs
+++ StreamsFilesAndDirectories-Exercise/DirectoryTraversal/Program.cs
@@ -9,23 +9,23 @@ namespace DirectoryTraversal
     {
         static void Main(string[] args)
         {
-            var fileInfo = new Dictionary<string, Dictionary<string, double>>();
+            var dictfileInfo = new Dictionary<string, Dictionary<string, double>>();
             DirectoryInfo directoryInfo = new DirectoryInfo(@"..\..\..\");
             var files = directoryInfo.GetFiles();
             foreach (var file in files)
             {
-                if (!fileInfo.ContainsKey(file.Extension))
+                if (!dictfileInfo.ContainsKey(file.Extension))
                 {
-                    fileInfo[file.Extension] = new Dictionary<string, double>();
+                    dictfileInfo[file.Extension] = new Dictionary<string, double>();
                 }
 
-                fileInfo[file.Extension].Add(file.Name, file.Length / 1024.00);
+                dictfileInfo[file.Extension].Add(file.Name, file.Length / 1024.00);
             }
 
             using (var writerInReport = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "report.txt"),true))
 
             {
-                foreach (var (extension, nameLength) in fileInfo.OrderByDescending(f => f.Value.Count()).ThenBy(x => x.Key))
+                foreach (var (extension, nameLength) in dictfileInfo.OrderByDescending(f => f.Value.Count()).ThenBy(x => x.Key))
                 {
                     writerInReport.WriteLine(extension);
                     foreach (var (name, length) in nameLength.OrderBy(f => f.Value))
