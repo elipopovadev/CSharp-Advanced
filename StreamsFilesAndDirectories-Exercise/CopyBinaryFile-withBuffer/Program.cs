f03bb16a160de39e00766c8e344b32d9bd4399f9
Elena Popova <elipopovadev@gmail.com>
2021-01-19 22:50:37 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Exercise/CopyBinaryFile-withBuffer/Program.cs
+++ StreamsFilesAndDirectories-Exercise/CopyBinaryFile-withBuffer/Program.cs
@@ -11,15 +11,10 @@ namespace CopyBinaryFile_withBuffer
             using (FileStream writer = new FileStream(@"..\..\..\newFile.png", FileMode.Create))
             {
                 byte[] buffer = new byte[4096];
-                while (reader.CanRead)
-                {
-                    int bytesRead = reader.Read(buffer, 0, buffer.Length);
-                    if (bytesRead == 0)
-                    {
-                        break;
-                    }
-
-                    writer.Write(buffer, 0, buffer.Length);
+                int bytesRead;
+                while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                {                 
+                    writer.Write(buffer, 0, bytesRead);
                 }
             }
         }
