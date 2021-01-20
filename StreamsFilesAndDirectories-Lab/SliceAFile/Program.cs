8b7ba27ff388540897bea076a23098a245d8d509
Elena Popova <elipopovadev@gmail.com>
2021-01-20 22:11:04 +0200

    SomeChanges

--- StreamsFilesAndDirectories-Lab/SliceAFile/Program.cs
+++ StreamsFilesAndDirectories-Lab/SliceAFile/Program.cs
@@ -8,15 +8,15 @@ namespace SliceAFile
     {
         static void Main(string[] args)
         {
-            using var readerInput= new FileStream("input.txt", FileMode.Open);
+            using var readerInput= new FileStream("input.txt", FileMode.Open); // open a FileStream
             var parts = 4;
             var length = (int)Math.Ceiling((decimal)readerInput.Length / parts);
             var buffer = new byte[length];
-            var bytestRead;
+            
             for (int i = 0; i < parts; i++)
             {
                 var bytesRead = readerInput.Read(buffer, 0, buffer.Length); // read the current bytes
-                using var writer = new FileStream($"Part-{i + 1}.txt", FileMode.Open); // create a FileStream
+                using var writer = new FileStream($"Part-{i + 1}.txt", FileMode.Open); // open a FileStream
                 writer.Write(buffer, 0, bytesRead); // write in a file
             }
         }
