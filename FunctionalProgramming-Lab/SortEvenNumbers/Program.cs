2477aba79d684c05e7edbda81adfcd7c86732da0
Elena Popova <elipopovadev@gmail.com>
2021-01-21 23:21:34 +0200

    SomeChanges

--- FunctionalProgramming-Lab/SortEvenNumbers/Program.cs
+++ FunctionalProgramming-Lab/SortEvenNumbers/Program.cs
@@ -12,10 +12,7 @@ namespace SortEvenNumbers
             foreach (var number in intArray)
             {
                 Console.Write(number + ", ");
-            }
-
-            
-
+            }           
         }
     }
 }
