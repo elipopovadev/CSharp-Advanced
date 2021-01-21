c11a2bfa776fe8a768a7bd41d205b86a63b28b15
Elena Popova <elipopovadev@gmail.com>
2021-01-22 00:38:57 +0200

    SomeChanges

---
+++ FunctionalProgramming-Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            Func<string, bool> func = x => char.IsUpper(x[0]);
+            var words = text.Split(" ",StringSplitOptions.RemoveEmptyEntries).Where(x => func(x));
+            Console.WriteLine(string.Join(Environment.NewLine,words));                   
+        }
+    }
+}
