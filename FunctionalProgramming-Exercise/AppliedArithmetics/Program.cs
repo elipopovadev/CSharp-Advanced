cd78bf41784c1130afda2dc847129c7846f5908e
Elena Popova <elipopovadev@gmail.com>
2021-01-26 00:18:15 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/AppliedArithmetics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
