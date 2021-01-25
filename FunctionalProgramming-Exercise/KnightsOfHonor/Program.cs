b42c7446943a467fd7102fac573d6af0a54b48dd
Elena Popova <elipopovadev@gmail.com>
2021-01-25 22:57:09 +0200

    SomeChanges

--- FunctionalProgramming-Exercise/KnightsOfHonor/Program.cs
+++ FunctionalProgramming-Exercise/KnightsOfHonor/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 
 namespace KnightsOfHonor
@@ -8,11 +7,9 @@ namespace KnightsOfHonor
     {
         static void Main(string[] args)
         {
-            string[] names = Console.ReadLine().Split();
-            Action<string[]> action = x => Console.WriteLine(string.Join (Environment.NewLine, "Sir" + " " + names.ToString()));
-            action(names);
-           
-
+            string[] names = Console.ReadLine().Split().Select(x=> "Sir "+ x).ToArray();
+            Action<string[]> actionPrint = x => Console.WriteLine(string.Join (Environment.NewLine, x));
+            actionPrint(names);         
         }
     }
 }
