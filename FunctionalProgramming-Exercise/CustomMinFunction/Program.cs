c33efaf524dc6f179d5e4ba463b88f5bb748e9c2
Elena Popova <elipopovadev@gmail.com>
2021-01-25 23:35:20 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> funcMinNumber = nums =>
+             {
+                 int minValue = int.MaxValue;
+                 foreach (var num in numbers)
+                 {
+                     if (num < minValue)
+                     {
+                         minValue = num;
+                     }
+                 }
+
+                 return minValue;
+             };
+
+            Console.WriteLine(funcMinNumber(numbers));
+        }
+    }
+}
--- FunctionalProgramming-Exercise/KnightsOfHonor/Program.cs
+++ FunctionalProgramming-Exercise/KnightsOfHonor/Program.cs
@@ -7,9 +7,9 @@ namespace KnightsOfHonor
     {
         static void Main(string[] args)
         {
-            string[] names = Console.ReadLine().Split().Select(x=> "Sir "+ x).ToArray();
-            Action<string[]> actionPrint = x => Console.WriteLine(string.Join (Environment.NewLine, x));
-            actionPrint(names);         
+            string[] names = Console.ReadLine().Split().Select(x => "Sir " + x).ToArray();
+            Action<string[]> actionPrint = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+            actionPrint(names);
         }
     }
 }
