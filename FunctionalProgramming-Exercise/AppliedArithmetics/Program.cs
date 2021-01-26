2527698fa515ec087669060fb97c26182cc0549a
Elena Popova <elipopovadev@gmail.com>
2021-01-26 17:15:39 +0200

    SomeChanges

--- FunctionalProgramming-Exercise/AppliedArithmetics/Program.cs
+++ FunctionalProgramming-Exercise/AppliedArithmetics/Program.cs
@@ -7,7 +7,33 @@ namespace AppliedArithmetics
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int, int> add = x => x + 1;
+            Func<int, int> multiply = x => x * 2;
+            Func<int, int> subtract = x => x - 1;
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                       numbers= numbers.Select(add).ToArray();
+                        break;
+                    case "multiply":
+                      numbers=  numbers.Select(multiply).ToArray();
+                        break;
+                    case "subtract":
+                       numbers= numbers.Select(subtract).ToArray();
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
