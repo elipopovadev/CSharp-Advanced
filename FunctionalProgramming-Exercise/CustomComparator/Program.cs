6b38e8db42ed9a30daac7c3f81c2f74472ce7ecf
Elena Popova <elipopovadev@gmail.com>
2021-01-26 19:41:57 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/CustomComparator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(numbers, (x, y) =>
+            {
+                int compare = 0;
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    compare = -1;
+                }
+
+                else if (x % 2 != 0 && y % 2 == 0)
+                {
+                    compare = 1;
+                }
+
+                else if (x < y)
+                {
+                    compare = -1;
+                }
+
+                else if (x > y)
+                {
+                    compare = 1;
+                }
+
+                return compare;
+
+            });
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ FunctionalProgramming-Exercise/PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split().ToArray();
+            Func<int, string[], string[]> func = (length, names) =>
+              {
+                  var newList = new List<string>();
+                  foreach (var name in names)
+                  {
+                      if(name.Length <= length)
+                      {
+                          newList.Add(name);
+                      }
+                  }
+
+                  return newList.ToArray();
+              };
+
+            var newNames = func(length, names);
+            Console.WriteLine(string.Join(Environment.NewLine, newNames));
+        }
+    }
+}
