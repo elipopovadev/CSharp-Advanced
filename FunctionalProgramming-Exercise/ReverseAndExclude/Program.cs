762a8c70512e0d7bbd03b2b6dbcbeebae109a0e6
Elena Popova <elipopovadev@gmail.com>
2021-01-26 17:54:39 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());          
+            var newArray= numbers.Reverse().Where(x => x % n != 0).ToArray();
+            Console.WriteLine(string.Join(" ", newArray));
+        }
+    }
+}
