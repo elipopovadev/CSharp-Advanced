fc74056fc11c7529f48a9a67c6e3309c40af65dc
Elena Popova <elipopovadev@gmail.com>
2021-01-27 18:04:39 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+        }
+    }
+}
