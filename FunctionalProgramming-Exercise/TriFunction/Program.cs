761845195ce4711c19550bc4031b390d5dc72a13
Elena Popova <elipopovadev@gmail.com>
2021-01-29 00:32:37 +0200

    SomeChanges

---
+++ FunctionalProgramming-Exercise/TriFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TriFunction
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Func<string, int, bool> myWhere = (name, n) => name.Length >= n;
+            
+            
+
+          
+        }
+    }
+}
