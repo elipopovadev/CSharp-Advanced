815cc793e4070863df1bdfdf2fc0723df1d70532
Elena Popova <elipopovadev@gmail.com>
2021-01-29 00:59:02 +0200

    SomeChanges

--- FunctionalProgramming-Exercise/TriFunction/Program.cs
+++ FunctionalProgramming-Exercise/TriFunction/Program.cs
@@ -10,11 +10,24 @@ namespace TriFunction
         {
             int n = int.Parse(Console.ReadLine());
             string[] names = Console.ReadLine().Split();
-            Func<string, int, bool> myWhere = (name, n) => name.Length >= n;
-            
-            
+            Func<string, int, bool> privateWhere = (name, n) => name.ToCharArray().Select(x => (int)x).Sum() >= n;
 
-          
+            Func<string[], string> getFirstValidName = names =>
+            {
+                var name = string.Empty;
+                foreach (var currentName in names)
+                {
+                    if (privateWhere(currentName, n))
+                    {
+                        name = currentName;
+                        break;
+                    }
+                }
+
+                return name;
+            };
+
+            Console.WriteLine(getFirstValidName(names));
         }
     }
 }
