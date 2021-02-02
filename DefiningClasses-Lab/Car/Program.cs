5b6dfacd5831ad690e32d5ae76e6f43a603b36e1
Elena Popova <elipopovadev@gmail.com>
2021-02-02 11:17:06 +0200

    SomeChanges

---
+++ DefiningClasses-Lab/Car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Vw";
+            car.Model = "MK3";
+            car.Year = 1992;
+            Console.WriteLine($"Make: {car.Make}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Year: {car.Year}");
+        }
+    }
+}
