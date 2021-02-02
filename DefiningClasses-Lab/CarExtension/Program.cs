8a7bd6f83eb4a98a1bd6c311b51e3aeef71742c0
Elena Popova <elipopovadev@gmail.com>
2021-02-02 12:50:41 +0200

    SomeChanges

---
+++ DefiningClasses-Lab/CarExtension/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarExtension
+{
+    public static class CarManufacturer
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(20);
+            Console.WriteLine(car.WhoAmI());                      
+        }
+    }
+}
