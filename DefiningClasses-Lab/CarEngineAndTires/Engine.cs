a2df698233c866c2986d0ddf2b0a7fec87b2c2f0
Elena Popova <elipopovadev@gmail.com>
2021-02-04 10:28:48 +0200

    SomeChanges

---
+++ DefiningClasses-Lab/CarEngineAndTires/Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarEngineAndTires
+{
+    class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+    }
+}
