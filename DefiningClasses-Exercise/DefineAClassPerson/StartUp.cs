f14a02118d79ae1dd65c7719c21dcd501af2edf3
Elena Popova <elipopovadev@gmail.com>
2021-02-05 11:27:15 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/DefineAClassPerson/StartUp.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DefineAClassPerson
+{
+    public static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstPerson = new Person("Pesho", 20);
+            var secondPerson = new Person("Gosho", 18);
+            var thirdPerson = new Person("Stamat", 43);
+        }
+    }
+}
