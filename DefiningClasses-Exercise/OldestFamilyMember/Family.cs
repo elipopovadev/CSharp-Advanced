dece603134cc7a3f9da39b2cf3fc742bac42a33b
Elena Popova <elipopovadev@gmail.com>
2021-02-05 14:02:54 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OldestFamilyMember
+{
+    public class Family
+    {
+
+        public Family()
+        {
+            this.FamilyList = new List<Person>();
+        }
+
+        public List<Person> FamilyList { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.FamilyList.Add(member);
+        }
+
+         public Person GetOldestMember()
+        {
+            var oldestPerson = this.FamilyList.OrderByDescending (person=> person.Age).FirstOrDefault();
+            return oldestPerson;          
+        }
+
+    }
+}
---
+++ DefiningClasses-Exercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OldestFamilyMember
+{
+    public static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var family = new Family();
+            int numberOfMembers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                string[] personArray = Console.ReadLine().Split();
+                string name = personArray[0];
+                int age = int.Parse(personArray[1]);
+                Person newPerson = new Person(name, age);
+                family.AddMember(newPerson);                                          
+            }
+
+            var oldestMember=family.GetOldestMember();
+            Console.Write($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
