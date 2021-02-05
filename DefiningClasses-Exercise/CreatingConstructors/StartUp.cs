46d351792266213b9687832542c1e453018e2296
Elena Popova <elipopovadev@gmail.com>
2021-02-05 11:59:00 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/CreatingConstructors/StartUp.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CreatingConstructors
+{
+    static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstPerson = new Person();
+            var secondPerson = new Person(20);
+            var thirdPerson = new Person("Stamat", 25);          
+        }
+    }
+}
