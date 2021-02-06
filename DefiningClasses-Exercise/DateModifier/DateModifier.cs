99d8c55d9e36908637ad6062085d89dcbc9834f6
Elena Popova <elipopovadev@gmail.com>
2021-02-06 17:55:23 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/DateModifier/DateModifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DateModifier
+{
+    class DateModifier
+    {
+        public DateModifier()
+        {
+            this.ListWithAllDays = new List<DateTime>();
+        }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DateTime> ListWithAllDays { get; set; }
+
+        public List<DateTime> ReturnDaysBetwenTwoDates(string startDate, string endDate)
+        {
+            this.StartDate = DateTime.Parse(startDate);
+            this.EndDate = DateTime.Parse(endDate);
+            if(this.StartDate < this.EndDate)
+            {
+                for (DateTime date = this.StartDate; date < this.EndDate; date = date.AddDays(1))
+                {
+                    this.ListWithAllDays.Add(date);
+                }
+            }
+
+            else
+            {
+                for (DateTime date = this.EndDate; date < this.StartDate; date = date.AddDays(1))
+                {
+                    this.ListWithAllDays.Add(date);
+                }
+            }
+          
+            return this.ListWithAllDays;
+        }
+    }
+}
---
+++ DefiningClasses-Exercise/DateModifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+            var dateModifier = new DateModifier();          
+            var listWithDays= dateModifier.ReturnDaysBetwenTwoDates(startDate, endDate);
+            Console.WriteLine(string.Join(" ", listWithDays.Count));
+        }
+    }
+}
