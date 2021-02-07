c704e33bdd5a92423f721b55afcbe3435ceadd7e
Elena Popova <elipopovadev@gmail.com>
2021-02-07 12:41:34 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/RawData/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+
+
+    }
+}
---
+++ DefiningClasses-Exercise/RawData/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RawData
+{
+    public static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
