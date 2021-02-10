959b10d75afb44091a68fdaff9fcb06bf818e567
Elena Popova <elipopovadev@gmail.com>
2021-02-10 09:36:25 +0200

    SomeChanges

---
+++ DefiningClasses-Exercise/PokemonTrainer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Program    
+    {
+        static void Main(string[] args)
+        {
+            var listWithTrainers = new List<Trainer>();
+            string firstCommand;
+            while ((firstCommand = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputArray = firstCommand.Split();
+                string trainerName = inputArray[0];
+                string pokemonName = inputArray[1];
+                string pokemonElement = inputArray[2];
+                int pokemonHealth = int.Parse(inputArray[3]);
+
+                var newPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                if (!listWithTrainers.Any(t => t.Name == trainerName))
+                {
+                    var newTrainer = new Trainer(trainerName);
+                    newTrainer.AddPokemon(newPokemon);
+                    listWithTrainers.Add(newTrainer);
+                }
+
+                else
+                {
+                    var findedTrainer = listWithTrainers.Where(t => t.Name == trainerName).First();
+                    findedTrainer.AddPokemon(newPokemon);
+                }
+            }
+
+            string secondCommand;
+            while ((secondCommand = Console.ReadLine()) != "End")
+            {
+                string element = secondCommand;
+                if (element == "Fire")
+                {
+                    foreach (var trainer in listWithTrainers)
+                    {
+                        trainer.CheckTrainerForAddBadge(element);
+                        trainer.CheckTrainerForReduceHealth(element);
+                    }
+                }
+
+                else if (element == "Water")
+                {
+                    foreach (var trainer in listWithTrainers)
+                    {
+                        trainer.CheckTrainerForAddBadge(element);
+                        trainer.CheckTrainerForReduceHealth(element);
+                    }
+                }
+
+                else if (element == "Electricity")
+                {
+                    foreach (var trainer in listWithTrainers)
+                    {
+                        trainer.CheckTrainerForAddBadge(element);
+                        trainer.CheckTrainerForReduceHealth(element);
+                    }
+                }
+
+                foreach (var trainer in listWithTrainers)
+                {
+                    trainer.CheckTrainerForDeadPokemons();
+                }
+            }
+
+            SortAndPrintResult(listWithTrainers);
+        }
+
+
+        private static void SortAndPrintResult(List<Trainer> listWithTrainers)
+        {
+            foreach (var trainer in listWithTrainers.OrderByDescending(t => t.NumberOfBadges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.CollectionOfPokemon.Count}");
+            }
+        }
+    }
+}
---
+++ DefiningClasses-Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.NumberOfBadges = 0;
+            this.CollectionOfPokemon = new List<Pokemon>();
+        }
+
+        public Trainer(string name, List<Pokemon> collectionOfPokemon)
+            :this(name)
+        {
+            this.NumberOfBadges = 0;
+            this.CollectionOfPokemon = collectionOfPokemon;
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> CollectionOfPokemon { get; set; }
+
+        public void AddPokemon(Pokemon pokemon)
+        {                     
+            this.CollectionOfPokemon.Add(pokemon);
+        }
+
+        public void CheckTrainerForAddBadge(string element)
+        {
+            if (this.CollectionOfPokemon.Any(p => p.Element == element))
+            {
+                this.NumberOfBadges++;
+            }
+        }
+        
+        public void CheckTrainerForReduceHealth(string element)
+        {
+            if (!this.CollectionOfPokemon.Any(p => p.Element == element))
+            {
+                foreach (Pokemon pokemon in this.CollectionOfPokemon)
+                {
+                    pokemon.Health -= 10;
+                }
+            }
+        }
+
+        public void CheckTrainerForDeadPokemons()
+        {
+            this.CollectionOfPokemon = this.CollectionOfPokemon.Where(pokemon => pokemon.Health > 0).ToList();
+        }
+    }
+}
