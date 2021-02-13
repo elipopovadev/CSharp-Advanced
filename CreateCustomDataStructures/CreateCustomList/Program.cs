f2447aaf1ef4395f535f6097f0e08917ec8c614f
Elena Popova <elipopovadev@gmail.com>
2021-02-13 20:15:46 +0200

    SomeChanges

--- CreateCustomDataStructures/CreateCustomList/Program.cs
+++ CreateCustomDataStructures/CreateCustomList/Program.cs
@@ -9,13 +9,11 @@ namespace CreateCustomList
             var customList = new CustomList();
             customList.Add(1);
             customList.Add(2);
-
             customList.Insert(2,10);
-            customList.Insert(1, 100);
-
+            customList.Insert(1,100);
             customList.RemoveAt(2);
-
-            Console.WriteLine(string.Join(" ",customList));           
+            customList.Swap(1,2);
+            Console.WriteLine(string.Join(" ",customList));  // 1 100 10         
         }
     }
 }
