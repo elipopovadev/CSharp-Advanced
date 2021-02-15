ea3b6df23b942cefbbfc86c1e589dc8cc49b58d5
Elena Popova <elipopovadev@gmail.com>
2021-02-15 15:09:05 +0200

    Add some test case

--- CreateCustomDataStructures/CreateLinkedList/Program.cs
+++ CreateCustomDataStructures/CreateLinkedList/Program.cs
@@ -8,13 +8,20 @@ namespace CreateLinkedList
         {
             var newLinkedList = new LinkedList();
             newLinkedList.AddFirst(1);
+            newLinkedList.RemoveFirst();
             newLinkedList.AddLast(2);
+            newLinkedList.AddFirst(1);
+            newLinkedList.RemoveLast();
+
             var array = newLinkedList.ToArray();
             foreach (var item in array)
             {
-                Console.WriteLine(item);
-
+                Console.WriteLine(item); // 1
             }
+            Console.WriteLine();
+            Console.WriteLine(newLinkedList.Head.Value); // 1
+            Console.WriteLine(newLinkedList.Tail.Value); // 1
+            Console.WriteLine($"Count {newLinkedList.Count}"); // 1
         }
     }
 }
