3d6099d8a511537d29c4ced4b3a6c512d0c2537e
Elena Popova <elipopovadev@gmail.com>
2021-02-15 14:57:12 +0200

    Create Link List

---
+++ CreateCustomDataStructures/CreateLinkedList/LinkedList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CreateLinkedList 
+{
+    class LinkedList : IEnumerable<int>
+    {
+        public LinkedList()
+        {
+            this.Head = default;
+            this.Tail = default;
+            this.Count = 0;
+        }
+
+        public Node Head { get; private set; }
+        public Node Tail { get;private set; }
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                var newNode = new Node(element, null);
+                this.Head = this.Tail = newNode;
+            }
+            else
+            {
+                var newNode = new Node(element, this.Head);
+                this.Head = newNode;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (this.Count == 0)
+            {
+                var newNode = new Node(element, null);
+                this.Head = this.Tail = newNode;
+            }
+            else
+            {
+                var newNode = new Node(element, null);
+                this.Tail.NextNode = newNode;
+                this.Tail = newNode;
+            }
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Head == this.Tail)
+            {
+                var currentNode = Head;
+                this.Head = default;
+                this.Tail = default;
+                this.Count = 0;
+                return currentNode.Value;
+            }
+            if (this.Count > 1 )
+            {
+                var previousHeadValue = this.Head.Value;
+                var nextNode = this.Head.NextNode;
+                this.Head = nextNode;
+                this.Count--;
+                return previousHeadValue;
+            }
+            else
+            {
+                throw new Exception("LinkedList is empty!");
+            }
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Head == this.Tail)
+            {
+                var currentNode = Head;
+                this.Head = default;
+                this.Tail = default;
+                this.Count = 0;
+                return currentNode.Value;
+            }
+            if (this.Count > 1)
+            {
+                var previousTail = this.Tail;
+                var currentNode = this.Head;
+                while (currentNode.NextNode != Tail)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+
+                currentNode.NextNode = null;
+                this.Count--;
+                this.Tail = currentNode;
+                return previousTail.Value;
+            }
+
+            else
+            {
+                throw new Exception("LinkedList is empty!");
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var currentNode = this.Head;
+            while (currentNode != null)
+            {            
+                yield return currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int[] ToArray()         
+        {
+            int[] newArray = new int[this.Count];
+            var currentNode = this.Head;
+            for (int counter = 0; counter < this.Count; counter++)
+            {
+                newArray[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+
+            return newArray;
+        }
+    }   
+}
---
+++ CreateCustomDataStructures/CreateLinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CreateLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var newLinkedList = new LinkedList();
+            newLinkedList.AddFirst(1);
+            newLinkedList.AddLast(2);
+            var array = newLinkedList.ToArray();
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
