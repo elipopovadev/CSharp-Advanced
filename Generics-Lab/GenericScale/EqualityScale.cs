d2e0783fb7f23677cfcb46450e42281fd6e1036b
Elena Popova <elipopovadev@gmail.com>
2021-02-19 13:56:55 +0200

    Generic Scale

---
+++ Generics-Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    class EqualityScale<T>
+    {
+        public EqualityScale(T first, T second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+
+        public T First { get; }
+        public T Second { get; }
+
+        public bool AreEqual()
+        {
+            return this.First.Equals(this.Second);
+        }
+    }
+}
---
+++ Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           var scale= new EqualityScale<string>("El", "El");
+            Console.WriteLine(scale.AreEqual());
+
+            var scalesecond = new EqualityScale<int>(10, 10);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
