c5dc83175c5775c31923bfd8e08a7e902e076102
Elena Popova <elipopovadev@gmail.com>
2021-02-19 12:56:17 +0200

    ArrayCreator

---
+++ Generics-Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericArrayCreator
+{
+    public  class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] newArray = new T[length];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = item;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ Generics-Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericArrayCreator
+{
+    public static class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5,"Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+    }
+}
