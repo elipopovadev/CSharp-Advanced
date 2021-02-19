03c8cce266f9ff51e923d102035b4e3fcaadcb3a
Elena Popova <elipopovadev@gmail.com>
2021-02-19 22:37:53 +0200

    Generic Box of String

---
+++ Generics -Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; }
+        public override string ToString()
+        {
+            return $"{Data.GetType().FullName}: {this.Data}";
+        }
+    }
+}
---
+++ Generics -Exercise/GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfString = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfString; i++)
+            {
+                string data = Console.ReadLine();
+                var newBox = new Box<string>(data);
+                Console.WriteLine(newBox.ToString());
+            }
+        }
+    }
+}
