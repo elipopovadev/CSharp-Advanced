a00a55a7642d81506c05bcbe180d44653e99d841
Elena Popova <elipopovadev@gmail.com>
2021-02-20 22:25:21 +0200

    SomeChanges

---
+++ Generics -Exercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
