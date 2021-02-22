12ac239d56230631739f9336473676ead6d0dc05
Elena Popova <elipopovadev@gmail.com>
2021-02-22 13:29:05 +0200

    Generic Count Method String

---
+++ Generics -Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace GenericCountMethodString
+{
+    class Box<T> : IComparable<Box<T>> where T : IComparable<T>
+    {
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; set; }
+
+        public int CompareTo(Box<T> otherBox)
+        {
+            return this.Data.CompareTo(otherBox.Data);
+        }
+
+        public override string ToString()
+        {
+            return $"{Data.GetType().FullName}: {this.Data}";
+        }
+    }
+}
--- Generics -Exercise/GenericCountMethodString/Program.cs
+++ Generics -Exercise/GenericCountMethodString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCountMethodString
 {
@@ -6,7 +7,35 @@ namespace GenericCountMethodString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            var listOfBoxes = new List<Box<string>>();
+            int numberOfElements = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                string element = Console.ReadLine();
+                var newBox = new Box<string>(element);
+                listOfBoxes.Add(newBox);
+            }
+
+            string elementForCompared = Console.ReadLine();
+            var BoxOfElementToCompare = new Box<string>(elementForCompared);
+            int count = CountGreaterValues(listOfBoxes, BoxOfElementToCompare);
+            Console.WriteLine(count);
+        }
+
+
+        public static int CountGreaterValues<T>(List<T> list, T elementToCompare)
+            where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (var box in list)
+            {
+                if (box.CompareTo(elementToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
