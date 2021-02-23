1bde238c052d0e47c76ae2ed249c57d7e6012204
Elena Popova <elipopovadev@gmail.com>
2021-02-23 19:17:48 +0200

    Threeuple

---
+++ Generics -Exercise/Threeuple/MyThreeuple.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Threeuple
+{
+    class MyThreeuple<T1, T2, T3>
+    {
+        public MyThreeuple(T1 firstElement, T2 secondElement, T3 thirdElement)
+        {
+            this.FirstElement = firstElement;
+            this.SecondElement = secondElement;
+            this.ThirdElement = thirdElement;
+        }
+
+        public T1 FirstElement { get; set; }
+        public T2 SecondElement { get; set; }
+        public T3 ThirdElement { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstElement} -> {this.SecondElement} -> {this.ThirdElement}";
+        }
+    }
+}
---
+++ Generics -Exercise/Threeuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namesAdressCity = Console.ReadLine().Split();
+            string firstName = namesAdressCity[0];
+            string lastName = namesAdressCity[1];
+            string names = firstName + " " + lastName;
+            string adress = namesAdressCity[2];
+            string city = string.Join(" ", namesAdressCity.Skip(3));
+            var threeupleNamesAdreessCity = new MyThreeuple<string, string, string>(names, adress, city);
+
+            string[] nameBeerDrunkOrNot = Console.ReadLine().Split();         
+            string name = nameBeerDrunkOrNot[0];
+            int beer = int.Parse( nameBeerDrunkOrNot[1]);
+            string condition = nameBeerDrunkOrNot[2];
+            bool checkIfCanDrunk = condition == "drunk" ? true : false;
+            var threeupleNameBeerChecker = new MyThreeuple<string, int, bool>(name, beer, checkIfCanDrunk);
+
+            string[] namesBalanceBank = Console.ReadLine().Split();
+            string firstNames = namesBalanceBank[0];
+            double balance = double.Parse(namesBalanceBank[1]);
+            string bank = namesBalanceBank[2];
+            var threeupleNamesBalanceBank = new MyThreeuple<string, double, string>(firstNames, balance, bank);
+
+            Console.WriteLine(threeupleNamesAdreessCity);
+            Console.WriteLine(threeupleNameBeerChecker);
+            Console.WriteLine(threeupleNamesBalanceBank);
+        }
+    }
+}
