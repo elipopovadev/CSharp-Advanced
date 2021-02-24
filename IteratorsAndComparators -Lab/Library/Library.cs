e7d54af7033deff9717952991299d845c8c9d722
Elena Popova <elipopovadev@gmail.com>
2021-02-24 22:36:22 +0200

    Library

---
+++ IteratorsAndComparators -Lab/Library/Library.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; }
+    }
+}
---
+++ IteratorsAndComparators -Lab/Library/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book firstBook = new Book("Animal Farm", 2003, "George Orwell");
+            Book secondBook = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book thirdBook= new Book("The Documents in the Case", 1930);
+
+            Library firstLibrary = new Library();
+            Library secondLibrary = new Library(firstBook, secondBook, thirdBook);
+        }
+    }
+}
