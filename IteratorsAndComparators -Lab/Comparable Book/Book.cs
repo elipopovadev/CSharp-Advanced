37af6cf70dff5f8c512bb0ba0781e2e8a220c06a
Elena Popova <elipopovadev@gmail.com>
2021-02-25 22:37:03 +0200

    Comparable Book

---
+++ IteratorsAndComparators -Lab/Comparable Book/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if(this.Year > other.Year)
+            {
+                return 1;
+            }
+
+            else if(this.Year < other.Year)
+            {
+                return -1;
+            }
+
+            else
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ IteratorsAndComparators -Lab/Comparable Book/Library.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = new HashSet<Book>(books);
+        }
+
+        public HashSet<Book> Books { get; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.Books.Reverse())
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator(); // always return this.GetEnumerator()
+        }            
+    }
+}
