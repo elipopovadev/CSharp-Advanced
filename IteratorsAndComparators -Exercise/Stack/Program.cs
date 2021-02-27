763be7c79442f6f8cb919366924bbeda37a769f7
Elena Popova <elipopovadev@gmail.com>
2021-02-27 20:27:47 +0200

    Stack

---
+++ IteratorsAndComparators -Exercise/Stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new stack();
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (command.Contains("Push"))
+                {
+                    string[] elements = command.Split(new char[] { ' ', ','},StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+                    int[] elementsInInt = elements.Select(int.Parse).ToArray();
+                    stack.Push(elementsInInt);
+                }
+
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }    
+        }
+    }
+}
---
+++ IteratorsAndComparators -Exercise/Stack/stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class stack : IEnumerable<int>
+    {
+        public stack()
+        {
+            this.List = new List<int>();
+        }
+        public List<int> List { get; }
+
+        public void Push(int[] list)
+        {
+            this.List.AddRange(list);
+        }
+
+        public int Pop()
+        {       
+            if (this.List.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid operation!");
+            }
+
+            else
+            {
+                int lastIndex = this.List.Count - 1;
+                int lastElement = this.List[lastIndex];
+                this.List.RemoveAt(lastIndex);
+                return lastElement;
+            }         
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = this.List.Count- 1; i >= 0; i--)
+            {
+                yield return this.List[i];
+            }
+
+            for (int i = this.List.Count - 1; i >= 0; i--)
+            {
+                yield return this.List[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
