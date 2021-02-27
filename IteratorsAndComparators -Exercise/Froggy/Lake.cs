d4ea2d87b32bdc1ef7cc2bbac48ae2f52b077847
Elena Popova <elipopovadev@gmail.com>
2021-02-27 22:23:13 +0200

    Froggy

---
+++ IteratorsAndComparators -Exercise/Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        public Lake(int[]stones)
+        {
+            this.Stones = new List<int>(stones);
+        }
+
+        public List<int> Stones { get; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.Stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return this.Stones[i];
+                }
+            }
+
+            for (int i = this.Stones.Count-1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return this.Stones[i];
+                }
+            }       
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
