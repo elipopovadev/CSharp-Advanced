72551936224396db9fa9f9ad7f61ddc8b9b99f33
Elena Popova <elipopovadev@gmail.com>
2021-02-28 21:56:55 +0200

    Comparing Objects

---
+++ IteratorsAndComparators -Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person: IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string Town { get; }
+
+        public int CompareTo( Person other)
+        {
+           if(this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+           else if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+
+           else if (this.Town.CompareTo(other.Town) != 0)
+            {
+               return this.Town.CompareTo(other.Town);
+            }
+
+            return 0;
+        }  
+    }
+}
---
+++ IteratorsAndComparators -Exercise/ComparingObjects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listWithPeople = new List<Person>();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] person = input.Split();
+                string name = person[0];
+                int age = int.Parse(person[1]);
+                string town = person[2];
+                var newPerson = new Person(name, age, town);
+                listWithPeople.Add(newPerson);
+            }
+
+            int indexToComparing = int.Parse(Console.ReadLine());
+            if(indexToComparing >= 0 && indexToComparing < listWithPeople.Count)
+            {
+                var personToComparing = listWithPeople[indexToComparing];
+                int countForEqualPeople = 0;
+                foreach (var person in listWithPeople)
+                {
+                    if (person.CompareTo(personToComparing) == 0)
+                    {
+                        countForEqualPeople++;
+                    }
+                }
+
+                int countForUnequalPeople = listWithPeople.Count - countForEqualPeople;
+                Console.WriteLine($"{countForEqualPeople} {countForUnequalPeople} {listWithPeople.Count}");
+            }
+
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
