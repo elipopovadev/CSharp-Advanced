744385ab3368dfd29bcf1a22776ab0c5860eba80
Elena Popova <elipopovadev@gmail.com>
2021-03-01 11:51:31 +0200

    EqualityLogic

---
+++ IteratorsAndComparators -Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    class Person : IComparable<Person>, IEquatable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set;}
+        public int Age { get; set;}
+
+        public int CompareTo( Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this.Age.GetHashCode();
+        }
+
+        public bool Equals( Person other)
+        {
+            return other != null && this.Name == other.Name
+                && this.Age == other.Age;
+        }
+    }
+}
---
+++ IteratorsAndComparators -Exercise/EqualityLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+            int numberOfPersons = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPersons; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person newPerson = new Person(name, age);
+                sortedSet.Add(newPerson);
+                hashSet.Add(newPerson);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
