8cf8c2f303de447fc8c1495bc809402900bff1ad
Elena Popova <elipopovadev@gmail.com>
2021-03-07 21:57:36 +0200

    Scheduling

---
+++ ExamPreparation/Scheduling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Scheduling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstLines = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondLines = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int valueToKill = int.Parse(Console.ReadLine());
+            var tasks = new Stack<int>(firstLines);
+            var threads = new Queue<int>(secondLines);
+
+            while(tasks.Count > 0 && threads.Count > 0)
+            {
+                int currentTask = tasks.Peek();
+                int currentThread = threads.Peek();
+
+                if(currentTask == valueToKill)
+                {
+                    Console.WriteLine($"Thread with value {currentThread} killed task {currentTask}");
+                    Console.WriteLine(string.Join(" ",threads));
+                    return;
+                }
+
+                else if(currentThread >= currentTask)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+
+                else if(currentThread < currentTask)
+                {
+                    threads.Dequeue();
+                }
+            }         
+        }
+    }
+}
